3b32904ce1ab95bcc0edb5e07b6f0ade99416651
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-18 17:15:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     elsif req.path.match(/items/)
 
       item_name = req.path.split("/items/").last
-      item = @@item.find{|i| i.name == item_name}
+      item = @@items.find{|i| i.name == item_name}
 
       if item
         resp.write item.price
